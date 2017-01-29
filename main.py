2ab664d05c29cfaf9ec3f9103c4ba7f80a036b10
Jason Pham <me@jasonpham.ca>
2017-01-29 09:22:45 -0500

    Finished learning

--- main.py
+++ main.py
@@ -3,12 +3,5 @@ import zipfile
 import nottingham_util
 import rnn
 
-# collect the data
-zip = zipfile.ZipFile(r'dataset.zip')  
-zip.extractall('data')  
-
-# build the model
-# nottingham_util.create_model()
-
-# train the model
-rnn.train_model()
+if __name__ == ‘__main__’:
+	rnn.train_model()
